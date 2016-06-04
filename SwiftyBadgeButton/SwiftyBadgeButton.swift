f54b2871a05f814641bdf7c1dfec09a759db1632
Raphael Cruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2016-06-04 03:16:04 +0100

    Positioning the badge

--- SwiftyBadgeButton/SwiftyBadgeButton.swift
+++ SwiftyBadgeButton/SwiftyBadgeButton.swift
@@ -10,6 +10,40 @@ import Foundation
 
 class SwiftyBadgeButton: UIButton {
 
-    let badgeView = UIView()
+    let badgeLabel = UILabel()
+    
+    var badgeSize = CGSize(width: 15, height: 15) {
+        didSet {
+            badgeLabel.frame.size = badgeSize
+            layoutSubviews()
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    init() {
+        super.init(frame: CGRectZero)
+        setup()
+    }
+    
+    private func setup() {
+        addSubview(badgeLabel)
+        
+        badgeLabel.frame.size = badgeSize
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        badgeLabel.frame.origin = CGPoint(x: bounds.size.width - badgeLabel.bounds.size.width / 2, y: bounds.size.height - badgeLabel.bounds.size.height / 2)
+    }
     
 }
--- SwiftyBadgeButtonTests/SwiftyBadgeButtonSpecs.swift
+++ SwiftyBadgeButtonTests/SwiftyBadgeButtonSpecs.swift
@@ -22,10 +22,21 @@ class SwiftyBadgeButtonSpecs: QuickSpec {
             beforeEach {
                 containerView =  UIView(frame: CGRect(x: 0, y: 0, width: 360, height: 1200))
                 sut = SwiftyBadgeButton(frame: CGRect(x: 10, y: 50, width: 40, height: 40))
+                containerView.addSubview(sut)
+                containerView.layoutIfNeeded()
             }
             
             it("should have the badgeView set") {
-                expect(sut.badgeView).toNot(beNil())
+                expect(sut.badgeLabel).toNot(beNil())
+            }
+            
+            it("should set the default size of the badge") {
+                expect(sut.badgeLabel.bounds.size).to(equal(CGSize(width: 15, height: 15)))
+            }
+            
+            it("should position the badge on the top right corner of the button") {
+                expect(sut.badgeLabel.frame.origin.x).to(beCloseTo(32.5, within: 0.1))
+                expect(sut.badgeLabel.frame.origin.y).to(beCloseTo(32.5, within: 0.1))
             }
             
         }
