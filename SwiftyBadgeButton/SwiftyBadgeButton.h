5ffc4ec9faf231638f00a43300b0c7544473eeb3
Raphael Cruzeiro <raphaelcruzeiro@raphaelcruzeiro.com>
2016-06-04 02:14:12 +0100

    Initial Commit

---
+++ SwiftyBadgeButton/SwiftyBadgeButton.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyBadgeButton.h
+//  SwiftyBadgeButton
+//
+//  Created by Raphael Cruzeiro on 04/06/16.
+//  Copyright © 2016 Raphael Cruzeiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyBadgeButton.
+FOUNDATION_EXPORT double SwiftyBadgeButtonVersionNumber;
+
+//! Project version string for SwiftyBadgeButton.
+FOUNDATION_EXPORT const unsigned char SwiftyBadgeButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyBadgeButton/PublicHeader.h>
+
+
